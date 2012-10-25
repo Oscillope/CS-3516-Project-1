7bbdc0b1fd5f7a698907b52320e6f7b5f7f56310
Louis Fogel <lafogel@lfbook.(none)>
2012-10-25 16:28:23 -0400

    Added client and server files

---
+++ client.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define DEFAULT_PORT "2012"
+int main(int argc, char** argv){
+    char *port; //Range from 0-65535 so five digits is always sufficient
+    port = DEFUALT_PORT;
+    struct addrinfo knowninfo;
+    struct addrinfo *servinfo;  // will point to the results
+
+    memset(&knowninfo, 0, sizeof knowninfo); // make sure the struct is empty
+    knowninfo.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+    knowninfo.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    knowninfo.ai_flags = AI_PASSIVE;     // fill in my IP for me
+}
---
+++ server.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define DEFAULT_PORT        "2012"
+#define DEFAULT_RATE_NUM    3
+#define DEFAULT_RATE_TIME   60
+#define DEFAULT_TIMEOUT     80
+#define DEFAULT_BACKLOG     20
+
+int main(int argc, char** argv){
+    char *port; //Range from 0-65535 so five digits is always sufficient
+    int ratenum, ratetime, timeout, backlog;
+    //TODO handle arguments;
+    //if not specified, set defaults
+    port = DEFAULT_PORT;
+    ratenum = DEFAULT_RATE_NUM;
+    ratetime = DEFAULT_RATE_TIME;
+    timeout = DEFAULT_TIMEOUT;
+    backlog = DEFAULT_BACKLOG;
+    int socketfd, acceptedfd;
+    struct sockaddr_storage newaddr;
+    socklen_t addrsize;
+    struct addrinfo knowninfo, *serverinfo;
+    //We wouldn't want crazy stack garbage ruining our sockets
+    memset(&knowninfo, 0, sizeof(knowninfo));
+    knowninfo.ai_family=AF_UNSPEC;
+    knowninfo.ai_socktype=SOCK_STREAM;
+    knowninfo.ai_flags=AI_PASSIVE;
+    if(getaddrinfo(NULL, port, &knowninfo, &serverinfo) != 0){
+        fprintf(stderr, "ERROR: getaddrinfo() returned with an error\n");
+        return 1;
+    }
+    socketfd = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
+    if(bind(socketfd, serverinfo->ai_addr, serverinfo->ai_addrlen)!=0){
+        fprintf(stderr, "ERROR: bind() returned with an error\n");
+        return 1;
+    }
+    if(listen(socketfd, backlog)!=0){
+        fprintf(stderr, "ERROR: listen() returned with an error\n");
+        return 1;
+    }
+    //TODO determine if we actually want to accept the connection
+    //accept incoming connection
+    //TODO pop up a thread to handle the connection
+    addrsize=sizeof(newaddr);
+    acceptedfd = accept(socketfd, (struct sockaddr *)&newaddr, &addrsize);
+    int *msg;
+    int rcvstatus = recv(acceptedfd, (void *)msg, sizeof(int), 0);
+    close(acceptedfd);
+    printf("Received %d from client!\n",*msg);
+    freeaddrinfo(serverinfo); // free up memory occupied by linked list
+}
