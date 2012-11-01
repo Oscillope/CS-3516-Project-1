2eae039d8c842cdf2853d25206036a418b738da5
Louis Fogel <lafogel@wpi.edu>
2012-11-01 14:12:31 -0400

    tried to make stuff work

--- client.c
+++ client.c
@@ -6,27 +6,28 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #define DEFAULT_PORT "2012"
-#define SERVER_ADDRESS "localhost"
+#define SERVER_ADDRESS "172.0.0.1"
 
 int main(int argc, char** argv){
-    char *port, *server; //Range from 0-65535 so five digits is always sufficient
+    char *port, *serveraddr; //Range from 0-65535 so five digits is always sufficient
     port = DEFAULT_PORT;
-    server = SERVER_ADDRESS;
+    serveraddr = SERVER_ADDRESS;
     struct addrinfo knowninfo;
     struct addrinfo *clientinfo;  // will point to the results
-
     memset(&knowninfo, 0, sizeof knowninfo); // make sure the struct is empty
     knowninfo.ai_family = AF_UNSPEC;
     knowninfo.ai_socktype = SOCK_STREAM;
     knowninfo.ai_flags = AI_PASSIVE;		// Use the host's IP
-    // Use localhost for now. TODO: change this to accept an argument
-    if(getaddrinfo(server, port, &knowninfo, &clientinfo) != 0) {
+    //knowninfo.ai_addr = address;// Use localhost for now. TODO: change this to accept an argument
+    if(getaddrinfo(serveraddr, port, &knowninfo, &clientinfo) != 0) {
 		fprintf(stderr, "FATAL: getaddrinfo() returned an error\n");
 		return 1;
 	}
 	int socketfd = socket(clientinfo->ai_family, clientinfo->ai_socktype, clientinfo->ai_protocol);
-    if(bind(socketfd, clientinfo->ai_addr, clientinfo->ai_addrlen) != 0){
+    if(bind(socketfd, clientinfo->ai_addr, clientinfo->ai_addrlen)!=0){
         fprintf(stderr, "FATAL: bind() returned an error\n");
         return 1;
     }
+    connect(socketfd, clientinfo->ai_addr, clientinfo->ai_addrlen);
+    return 0;
 }
